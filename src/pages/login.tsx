8aac57b8c06edd388917b499fb46349b97b6623f
Andre Luiz Pastore Dambros <andreluizpd@gmail.com>
2021-08-08 22:19:55 -0300

    Add register and login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,24 +4,22 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { InputField } from '../components/InputField';
 import { Wrapper } from '../components/Wrapper';
-import { useRegisterMutation } from '../generated/graphql';
+import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 
-interface registerProps {}
-
-const Register: React.FC<registerProps> = ({}) => {
+const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
-  const [, register] = useRegisterMutation();
+  const [, login] = useLoginMutation();
 
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
             router.push('/');
           }
         }}
@@ -42,7 +40,7 @@ const Register: React.FC<registerProps> = ({}) => {
               ></InputField>
             </Box>
             <Button type='submit' color='teal' mt={4} isLoading={isSubmitting}>
-              Submit
+              Login
             </Button>
           </Form>
         )}
@@ -51,4 +49,4 @@ const Register: React.FC<registerProps> = ({}) => {
   );
 };
 
-export default Register;
+export default Login;
