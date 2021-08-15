64f3333c99219ae96d68bbcb9ced4fdd07a24a53
Andre Luiz Pastore dambros <andreluizpd@gmail.com>
2021-08-15 12:06:32 -0300

    Feat: Added create post

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,6 +3,7 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from '@chakra-ui/react';
 import { useField } from 'formik';
 import React from 'react';
@@ -10,19 +11,27 @@ import React from 'react';
 type InputFieldProps = JSX.IntrinsicElements['input'] & {
   label: string;
   name: string;
+  textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextArea: any = Input;
+
+  if (textarea) {
+    InputOrTextArea = Textarea;
+  }
+
   const [field, { error }] = useField(props);
 
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputOrTextArea {...field} {...props} id={field.name} />
       <FormErrorMessage>{error}</FormErrorMessage>
     </FormControl>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
-import { NavBar } from '../components/NavBar';
 import { withUrqlClient } from 'next-urql';
 import { CreateUrqlClient } from '../utils/CreateUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
+import { Layout } from '../components/Layout';
+import { Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
 
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <NextLink href='/create-post'>
+        <Link>Create Post</Link>
+      </NextLink>
       <div>Hello World</div>
       <hr />
       {!data ? (
@@ -16,7 +20,7 @@ const Index = () => {
       ) : (
         data.posts.map(p => <div key={p.id}>{p.title}</div>)
       )}
-    </>
+    </Layout>
   );
 };
 
